d541f8e2230d87e91f90a7591db77cb6c6a5977e
GX-Codes <help.heeratelecom9@gmail.com>
2025-07-19 18:56:49 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
-print("hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    print("hello")  # this will also print in the Render logs
+    return "Hello from Python on Render!"
+
+if __name__ == "__main__":
+    app.run()
